c1fb984c19ddd2d718ed83409614d53284495057
Freddy Kelly <me@fredkelly.net>
2012-10-04 00:04:15 +0100

    Probably full of bugs but I'm tired..

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ pointhq-updater
 ===============
 
 Automatically updates Point HQ A-records.
+
+## Usage:
+Simply run `ruby updater.rb` and enter your `username` and `apitoken`, then select which zone/record you wish to perform updates on. The script will generate a `config.yml` in the working directory which will save all this information for next time you run (i.e. via `cron`). If you wish to change zone/record run `ruby updater.rb --update`.
+
+## Credits
+Uses the [point](https://rubygems.org/gems/point) gem by Adam Cooke, as well as [highline](https://rubygems.org/gems/highline) for IO.
+
+## Notes
+This (like most things I make) is pretty rough around the edges. I wrote this for quite a specific use case, please feel free to fork/tidy/fix/add etc. etc. :)
---
+++ updater.rb
@@ -0,0 +1,87 @@
+require 'yaml'
+require 'point'
+require 'net/http'
+require 'highline/import'
+
+#------------------------------------------------------------
+
+# add nice to_s to record
+class Point::ZoneRecord
+  def to_s
+    "#{record_type}\t#{name}\t#{data}"
+  end
+end
+
+#------------------------------------------------------------
+
+# are we updating the config?
+update = ARGV.first == '--update'
+
+# grab config file
+config = YAML::load_file('./config.yml') rescue {}
+
+# get api details
+point_username = config[:point_username] || ask('username:')
+point_apitoken = config[:point_apitoken] || ask('api token:')
+
+# setup point api connection
+Point.username = point_username
+Point.apitoken = point_apitoken
+
+# if not configured..
+if config.empty? || update
+  # get available zones
+  zones = Point::Zone.find(:all)
+
+  puts "Zones for \"#{point_username}\".."
+
+  # choose target zone
+  target_zone = nil
+  choose do |menu|
+    menu.prompt = 'Please select target zone..'
+    zones.each do |zone|
+      menu.choice(zone.name) { target_zone = zone }
+    end
+  end
+
+  puts "Records for \"#{target_zone.name}..\""
+
+  # choose target record
+  target_record = nil
+  choose do |menu|
+    menu.prompt = 'Please select target record..'
+    target_zone.records.each do |record|
+      menu.choice(record) { target_record = record }
+    end
+  end
+
+  # create config
+  config = {
+    :point_username => point_username,
+    :point_apitoken => point_apitoken,
+    :target_zone    => target_zone.id,
+    :target_record  => target_record.id
+  }
+  
+  # save it!
+  File.open('./config.yml', 'w') do |file|
+    YAML.dump(config, file)
+  end
+else
+  # user configured values
+  target_zone = Point::Zone.find(config[:target_zone])
+  target_record = target_zone.record(config[:target_record])
+end
+
+#------------------------------------------------------------
+
+# get your external ip address
+my_ip = Net::HTTP.get('ip.appspot.com', '/').chomp
+
+# set record to your ip
+target_record.data = my_ip
+
+# save it!
+if target_record.save
+  puts "Updated \"#{target_record.name}\" -> #{target_record.data} :-)"
+end
